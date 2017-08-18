d616a6bbd10bbcea4ecaf5274b0c9d8b3af54181
ixuhan <964850509@qq.com>
2017-08-18 18:36:21 +0800

    修改算法文件

--- Algorithm.py
+++ Algorithm.py
@@ -1,12 +1,15 @@
 #二分查找
-def binSearch( n,a):
+def binSearch(n,a):
     lo = 0
-    hi = int(len(a) - 1)
-    while(lo <= hi):
-        mid = int((lo + hi) / 2)
-        if n < a[mid]:   hi = mid - 1
-        elif n > a[mid]: lo = mid + 1
-        else: return mid
+    hi = len(a) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if n < a[mid]:
+            hi = mid - 1
+        elif n > a[mid]:
+            lo = mid + 1
+        else:
+            return mid
     return -1
 
 #选择排序，缺点：如果已经有序，仍然要做N^2/2次比较
@@ -17,8 +20,6 @@ def chooseSort(a):
             if a[min] > a[j]: #找到最小位置的下标
                 min = j
         a[i],a[min] = a[min],a[i] #交换首位和最小位
-    for i,val in enumerate(a): #打印结果数组
-        print (val,' ',end='')
 
 #插入排序
 def insertSort(a):
@@ -26,8 +27,17 @@ def insertSort(a):
         for j in range(i,0,-1): #由于前面已经有序，则只需要冒泡一次
             if a[j] < a[j - 1]:
                 a[j],a[j-1] = a[j-1],a[j]
-    for i,val in enumerate(a): #打印结果数组
-        print (val,' ',end='')
 
+#希尔排序
+def shellSort(a):
+    gap = len(a) // 2 #希尔的分组值
+    while gap != 0:
+        for i in range(gap):
+            for j in range(i + gap,len(a) - gap,gap):
+                if a[j] < a[j + gap]:
+                    a[j],a[j+gap] = a[j+gap],a[j]
+        gap //= 2
 
-insertSort([1,5,3,6,5,4,7,8,9,2])
+num = [1,5,3,6,5,4,7,8,9,2]
+shellSort(num)
+print(num)
